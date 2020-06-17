7493ccb6d6c4a4b7be08bb1aef672e3450767979
marianabastarracheas@gmail.com <marianabastarrachea@MacBook-Pro-2.local>
2020-06-16 20:23:59 -0500

    carusel

--- src/components/Carrousell.js
+++ src/components/Carrousell.js
@@ -1,29 +1,36 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel'
 
 const Carrousell = () => {
     return (
-        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-            <div className="carousel-inner corregir-imagen">
-                <div className="carousel-item active">
-                    <img src="https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__480.jpg" className="d-block w-100" alt="Foto de pixabay"></img>
-                </div>
-                    <div className="carousel-item">
-                        <img src="https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521__480.jpg" className="d-block w-100" alt="Foto de pixabay"></img>
-                    </div>
-                        <div className="carousel-item">
-                            <img src="https://cdn.pixabay.com/photo/2016/03/27/18/27/sky-1283446__480.jpg" className="d-block w-100" alt="foto de pixabay"></img>
-                        </div>
-                        </div>
-                        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Previous</span>
-                        </a>
-                        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Next</span>
-                        </a>
-                    </div>
+            <Carousel>
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src="https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_1280.jpg"
+                alt="First slide"
+            />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src="https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg"
+                alt="Third slide"
+            />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src="https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074_1280.jpg"
+                alt="Third slide"
+            />
+        </Carousel.Item>
+    </Carousel>
+       
     )
 }
+export default Carrousell
 
-export default Carrousell;
+
+    
+
